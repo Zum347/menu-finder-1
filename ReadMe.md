3d701719d6f3eb3700dfd0119a127c9981391069
zumrea777 <acarzmrete@yandex.com>
2024-06-27 20:54:05 +0300

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+# Menu Finder
+
+. Bu proje HTML, CSS, Bootstrap ve JS , kullanılarak oluşturulan bir menü sitesidir.
+
+## Kütüphaneler
+
+- cdn için : [https://cdnjs.com/]  
+- button için :[https://getbootstrap.com/docs/4.0/components/buttons/]
---
+++ app.js
@@ -0,0 +1,81 @@
+//console.log('Bağlantı Kontrol')
+import menu from "./database.js";
+//console.log(menu)
+const menuContainer = document.getElementById("menu-container");
+//console.log(menuContainer)
+
+const filterButton = document.querySelectorAll(".filter-btn");
+//console.log(filterButton)
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  displayMenu(menu);
+});
+
+function displayMenu(menuItems) {
+  // console.log(menuItems);
+  let dispMenu = menuItems.map(
+    (menuItem) => `
+ <div
+ class="d-flex align-items-center gap-3 flex-column flex-md-row my-2"
+ id="card"
+>
+ <img
+   src=${menuItem.img}
+   alt=""
+   id="image"
+   class="rounded shadow"
+ />
+ <div>
+   <div class="d-flex justify-content-between">
+     <h5>${menuItem.title}</h5>
+     <p>${menuItem.price} &#8378;</p>
+   </div>
+   <p class="lead">
+     ${menuItem.desc}
+   </p>
+ </div>
+</div>
+ `
+  );
+  dispMenu = dispMenu.join("");
+  menuContainer.innerHTML = dispMenu;
+}
+
+
+filterButton.forEach((button) => {
+  //console.log(button)
+
+  
+  button.addEventListener("click", (e) => {
+    
+    const category = e.target.dataset.id;
+    //console.log(category);
+    searchCategory(category);
+  });
+});
+
+//filtreleme fonksiyonu
+function searchCategory(categoryInfo) {
+  //console.log(categoryInfo);
+  
+  const filteredMenu = menu.filter(
+    
+    (menuItem) => menuItem.category === categoryInfo
+  );
+
+  //console.log(filteredMenu)
+
+  if (categoryInfo === "all") {
+    
+    displayMenu(menu);
+
+  }
+  
+  else {
+    
+    displayMenu(filteredMenu);
+  }
+}
+
